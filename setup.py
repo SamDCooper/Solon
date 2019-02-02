41eee448ac0c2fd8a0095603b2c556610b384cc7
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-02 18:19:50 +0000

    Initial setup of library with basic requirements and package info - very bare bones

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="Solon",
+    version="0.0.1",
+    description="discord.py wrapper for bot creation",
+    author="Falsely True Bots",
+    author_email="FalselyTrueBots@users.noreply.github.com",
+    url="https://github.com/FalselyTrueBots",
+    packages=["solon"],
+    )
---
+++ solon/__init__.py
@@ -0,0 +1,7 @@
+import logging
+
+log = logging.getLogger(__name__)
+log.debug(f"Initializing {__name__}")
+
+def hello_world():
+    print("Hello, world!")
