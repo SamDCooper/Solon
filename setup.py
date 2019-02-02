c2cc2304cef4c70cf51d4388454eba26e6bdb7b1
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-02 19:29:47 +0000

    more playing with setup

--- setup.py
+++ setup.py
@@ -13,4 +13,5 @@ setuptools.setup(
     url="https://github.com/FalselyTrueBots",
     packages=["solon"],
     install_requires=requirements,
+    dependency_links=["git+ssh://git@github.com/Rapptz/discord.py.git@1222bce271cf736b4db8c1eecb2823edd22f85dc#egg=discord.py"]
     )
