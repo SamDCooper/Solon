1d33a58c72bf9dc084c30e5f08ec3ce899d35c31
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-02-09 17:27:18 +0000

    Deserialization test working.. very basic

--- tests/test_deserialization.py
+++ tests/test_deserialization.py
@@ -9,11 +9,25 @@ log = logging.getLogger(__name__)
 
 test_passed = False
 
+config = solon.get_config("solon.tests")
 
 class MyClient(discord.Client):
+    def __init__(self, test_case):
+        super().__init__()
+        self.test_case = test_case
+
     async def on_ready(self):
-        global test_passed
-        test_passed = True
+        guild = self.get_guild(config["testing_guild_id"])
+        self.test_case.assertTrue(guild is not None)
+        if guild:
+            sd = solon.SerializedData(
+                value_serialized="@everyone",
+                type_name="role"
+            )
+
+            role = solon.deserialize(serialized_data=sd, guild=guild)
+            self.test_case.assertTrue(role is not None)
+
         await self.logout()
 
 
@@ -21,14 +35,13 @@ class TestDeserialization(unittest.TestCase):
     """Tests deserialization"""
 
     def test_deserialization(self):
-        #bot = MyClient()
-        #bot.run(solon.get_config("token"))
-        self.assertTrue(True)
-        #self.assertTrue(test_passed)
+        bot = MyClient(self)
 
-        #while not bot.is_closed():
-        #    pass
-        #bot.clear()
+        async def do_test():
+            await bot.login(solon.get_config("token"))
+            await bot.connect(reconnect=False)
+
+        bot.loop.run_until_complete(do_test())
 
 
 if __name__ == "__main__":
