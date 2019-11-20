782f7fc407b0e8f626cb1bfd50c4caca75cfe8bc
Falsely True <FalselyTrueBots@users.noreply.github.com>
2019-11-20 19:39:04 +0000

    Update to v8 - update discord.py to 1.3

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="Solon",
-    version="0.0.7",
+    version="0.0.8",
     description="discord.py wrapper for bot creation",
     author="Falsely True Bots",
     author_email="FalselyTrueBots@users.noreply.github.com",
     url="https://github.com/FalselyTrueBots",
     packages=["solon"],
     install_requires=[
-        "discord.py @ git+ssh://git@github.com/Rapptz/discord.py@1222bce271cf736b4db8c1eecb2823edd22f85dc#egg=discord.py-1.0.0a",
+        "discord.py @ git+ssh://git@github.com/Rapptz/discord.py@4ef0fb0d959b69d3b2cea87b0fc08310028e348f#egg=discord.py-1.3.0a",
         "emoji"
     ]
 )
